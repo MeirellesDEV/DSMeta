f6e606bdebcad9c165f49997d600a252eb777b68
Liandra Meirelles <liameirelles.dev@gmail.com>
2022-07-17 22:34:11 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 import icon from "../../assets/img/Vector.svg";
 import { BASE_URL } from "../../utils/request";
 import "./styles.css";
@@ -10,10 +11,9 @@ type Props = {
 function handleClick(id : number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
     .then(response => {
-        console.log("SUCESSO");
+        toast.info("SMS enviado com sucesso!");
     })
 }
-
 
 function NotificationButton( {saleId} : Props) {
     return (
